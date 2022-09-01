11008c7a337f1c242dee60ab19376dc72b12644b
Egor Obukhov <egor.obukhov@x5.ru>
2022-09-01 18:56:02 +0300

    cli auth for check ip pw login

---
+++ cmd/cli/auth.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/WHGLD/antibruteforce/internal/server"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(authCmd)
+}
+
+var authCmd = &cobra.Command{
+	Use:  "auth <login> <pass> <ip>",
+	Long: "Auth command - check for given login pw and ip",
+	Args: cobra.ExactArgs(3),
+	Run:  Auth,
+}
+
+func Auth(cmd *cobra.Command, args []string) {
+	login := args[0]
+	pw := args[1]
+	ip := args[2]
+
+	client, err := getGRPCClient()
+	if err != nil {
+		fmt.Println("Connection refused")
+		os.Exit(504)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	response := &server.AuthResponse{Ok: false}
+	response, err = client.Auth(ctx, &server.AuthRequest{Login: login, Password: pw, Ip: ip})
+
+	cancel()
+
+	if err != nil {
+		fmt.Printf(
+			"error while auth [login: %s password: %s ip: %s]: %s\n",
+			login,
+			pw,
+			ip,
+			err.Error(),
+		)
+		os.Exit(13)
+	}
+	fmt.Printf(
+		"auth [login: %s password: %s ip: %s] ok - %t\n",
+		login,
+		pw,
+		ip,
+		response.GetOk(),
+	)
+}
